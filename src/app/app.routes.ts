355a5db6ae7e60955d25b52e93dbacd5b96f7d37
Huitzitzili Leon Nieves <2668459@tcs.com>
2025-12-22 16:37:27 -0600

    ADD_Feature guard, routes, services (Auth)

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,5 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'login', loadComponent: () => import('./components/login/auth/auth').then(m => m.Auth)},
+]
--- src/app/services/auth/auth.service.ts
+++ src/app/services/auth/auth.service.ts
@@ -1,8 +1,32 @@
-import { Injectable } from '@angular/core';
+import { computed, Injectable, signal } from '@angular/core'
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
   
+  _isAuthenticated =  signal<boolean>(false)
+
+  public isAuthenticated =  computed(() =>  this._isAuthenticated())
+
+  login(password: string): boolean{
+    if(password === "123456780"){
+      this._isAuthenticated.set(true)
+      localStorage.setItem('token','true')
+      return true
+    }
+    return false
+  }
+
+  logOut(){
+    this._isAuthenticated.set(false)
+    localStorage.removeItem('token')
+  }
+
+  checkoutStatus(){
+    let token =  localStorage.getItem('token')
+    this._isAuthenticated.set(!!token)
+  }
+
+
 }
