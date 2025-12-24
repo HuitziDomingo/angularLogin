2cb421043948328aa5a8d777c083596de10b68fc
Huitzitzili Leon Nieves <2668459@tcs.com>
2025-12-24 13:12:26 -0600

    CREATED all files created and configurated

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,22 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
-    {path: 'login', loadComponent: () => import('./components/login/auth/auth').then(m => m.Auth)},
+    {
+        path: 'login',
+        loadComponent: () => import('./components/login/auth/auth').then(m => m.Auth)
+    },
+    {
+        path: 'dashboard',
+        loadComponent: () => import('./components/dashboard/dashboard/dashboard').then(m => m.Dashboard)
+    },
+    {
+        path: '',
+        redirectTo: 'login',
+        pathMatch: 'full'
+    },
+    {
+        path: '**',
+        redirectTo: 'login',
+
+    }
 ]
--- src/app/components/login/login/login.ts
+++ src/app/components/login/login/login.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import { AuthService } from '../../../services/auth/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -7,5 +9,18 @@ import { Component } from '@angular/core';
   styleUrl: './login.scss',
 })
 export class Login {
+
+  private authService = inject(AuthService)
+  private router = inject(Router)
+
+  password!: string
+  errorMessage = signal<string>('')
+
+  onLogin() {
+    let success =  this.authService.login(this.password)
+    if(success)
+      this.router.navigate(['/dashboard'])
+    else this.errorMessage.set('Credenciales incorrectas')
+  }
 
 }
